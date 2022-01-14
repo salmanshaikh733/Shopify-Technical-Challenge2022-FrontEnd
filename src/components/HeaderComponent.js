022bd562744a52752748d5bde15b1303b4a8b82c
Salman <salman.shaikh1678@gmail.com>
2022-01-13 22:10:24 -0500

    committing changes to master branch

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class HeaderComponent extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default HeaderComponent;

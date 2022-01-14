ec17a48882a2d613f6905902b1d9d23941cb5a34
Salman <salman.shaikh1678@gmail.com>
2022-01-14 01:38:29 -0500

    button to go to the employee add form page

--- src/components/ListEmployeeComponent.js
+++ src/components/ListEmployeeComponent.js
@@ -1,69 +1,53 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import EmployeeService from "../services/EmployeeService";
+import {useNavigate} from "react-router";
+
 //TODO add routing from add button
 
 function ListEmployeeComponent() {
+    let navigate = useNavigate();
+    const [employees, setEmployees] = useState([])
 
-  /*  constructor(props) {
-        super(props);
-        this.state = {
-            employees: []
-        }
-
-        this.addEmployee= this.addEmployee.bind(this);
-    }*/
-
-    const[employees, setEmployees] = useState([])
-
-    /*addEmployee(){
-        this.props.history.push('/add-employee');
-    }*/
-
-   /* componentDidMount() {
-        EmployeeService.getEmployees().then((res)=> {
-            this.setState({employees:res.data})
-        });
-    }*/
-
-    useEffect(()=>{
-        EmployeeService.getEmployees().then((res)=> {
+    //on initial mount
+    useEffect(() => {
+        EmployeeService.getEmployees().then((res) => {
             setEmployees(res.data)
         })
-    },[])
+    }, [])
 
 
-        return (
-            <div>
-                <h2 className="text-center">Employees List</h2>
-                {/*<button className="btn btn-primary button" onClick={() => history.push('/add-employee')}> Add Employee</button>*/}
-                <div className="row">
-                </div>
-                <div className="row">
-                    <table className="table table-striped table-bordered">
-                        <thead>
-                          <tr>
-                             <th> Employee First Name</th>
-                             <th> Employee Last Name</th>
-                             <th> Employee Email Id</th>
-                             <th> Actions </th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            employees.map(
-                                employee => <tr key={employee.id}>
-                                            <td> {employee.firstName}</td>
-                                            <td> {employee.lastName}</td>
-                                            <td> {employee.emailId}</td>
-                                </tr>
-                            )
-                        }
-                        </tbody>
-                    </table>
-                </div>
-
+    return (
+        <div>
+            <h2 className="text-center">Employees List</h2>
+            <button className="btn btn-primary button" onClick={() => navigate("/add-employee")}> Add Employee</button>
+            <div className="row">
             </div>
-        );
+            <div className="row">
+                <table className="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th> Employee First Name</th>
+                        <th> Employee Last Name</th>
+                        <th> Employee Email Id</th>
+                        <th> Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        employees.map(
+                            employee => <tr key={employee.id}>
+                                <td> {employee.firstName}</td>
+                                <td> {employee.lastName}</td>
+                                <td> {employee.emailId}</td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    );
 }
 
 export default ListEmployeeComponent;

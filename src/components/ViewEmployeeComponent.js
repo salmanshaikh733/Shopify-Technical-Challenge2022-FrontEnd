2c85cf0bd3b3f8185c64610ad9296c15b646e864
Salman <salman.shaikh1678@gmail.com>
2022-01-15 00:09:33 -0500

    finished view component

--- src/components/ViewEmployeeComponent.js
+++ src/components/ViewEmployeeComponent.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import EmployeeService from "../services/EmployeeService";
+import {useNavigate} from "react-router";
+
 
 function ViewEmployeeComponent(props) {
+    let navigate = useNavigate();
+
+    const params = useParams();
+    const [employeeId, setEmployeeId] = useState(params.id);
+    let [employee, setEmployee] = useState();
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [emailId, setEmailId] = useState("")
+
+
+    //component did mount
+    useEffect(() => {
+        EmployeeService.getEmployeeById(employeeId).then((res) => {
+            employee = res.data;
+            setFirstName(employee.firstName)
+            setLastName(employee.lastName)
+            setEmailId(employee.emailId)
+
+        })
+    }, [])
+
+    const goBack = event => {
+        navigate("/employees");
+    }
+
     return (
         <div>
-
-
-
+            <div className="card col-md-6 offset-md-3 margin-top">
+                <h3 className="text-center"> View Employee Details </h3>
+                <div className="card-body">
+                    <div className="row">
+                        <label>Employee First Name: {firstName}</label>
+                    </div>
+                    &nbsp;
+                    <div className="row">
+                        <label>Employee Last Name: {lastName}</label>
+                    </div>
+                    &nbsp;
+                    <div className="row">
+                        <label>Employee Email : {emailId}</label>
+                    </div>
+                </div>
+                <div >
+                    <button type="submit" className="btn btn-danger go-back-button" onClick={goBack}>Go Back</button>
+                </div>
+            </div>
         </div>
     );
 }
